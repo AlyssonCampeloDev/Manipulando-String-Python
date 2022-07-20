2234e7121d7650b469cf2eeedc447ab7a00e6fbe
Alysson Campelo <87710684+AlyssonCampeloDev@users.noreply.github.com>
2022-07-20 11:19:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+frase = 'softex meu amor'
+print('A quantidade de letras O é:', frase.count('o'))
+
+resultado = 'estudando até ser aprovado'
+print('A quantidade de caracteres é:', len(resultado.strip()))
+
+aprimorando = 'tenho certeza que irei ser reprovado!'
+print(aprimorando.replace('reprovado', 'aprovado'))
